bf7b32e6e5d5c266bf4a7b3b689325d1ceaf5d8b
Jacob Babich <babichjacob@Jacobs-Mac.local>
2020-03-24 04:26:56 -0400

    🎨 style: reformat all the files based on these new ESLint rules

--- src/routes/example.ts
+++ src/routes/example.ts
@@ -1,9 +1,11 @@
-import {Request as ExpressRequest, Response as ExpressResponse} from "express";
+import { Request as ExpressRequest, Response as ExpressResponse } from "express";
 
-export const get = async (req: ExpressRequest, res: ExpressResponse): Promise<void> => { // eslint-disable-line require-await
+// eslint-disable-next-line max-len
+export const get = async (req: ExpressRequest, res: ExpressResponse): Promise<void> => {
 	res.end("you made a get request");
 };
 
-export const post = async (req: ExpressRequest, res: ExpressResponse): Promise<void> => { // eslint-disable-line require-await
+// eslint-disable-next-line max-len
+export const post = async (req: ExpressRequest, res: ExpressResponse): Promise<void> => {
 	res.end("you made a post request");
 };
